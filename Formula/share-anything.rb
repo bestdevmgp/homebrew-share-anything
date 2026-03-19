650c7d4b60471beef1fd78829ba6764d77a60493
bestdevmgp <me@mingyu.dev>
2026-03-19 22:03:35 +0900

    chore: modify keyword

--- Formula/share-anything.rb
+++ Formula/share-anything.rb
@@ -1,46 +1,46 @@
 class ShareAnything < Formula
   desc "CLI for Share Anything - fast file sharing from the terminal"
   homepage "https://github.com/bestdevmgp/share-anything-cli"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/sany-macos-aarch64"
-      sha256 "c915a317aa97d7c8d15055b90942c9f7a902b3f901c4729b107aa10759950597"
+      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/share-macos-aarch64"
+      sha256 "975dc95d29f10deab259341a1d5c308add3f4518f70dc7ac7ff1ac67e8b54499"
 
       def install
-        bin.install "sany-macos-aarch64" => "sany"
+        bin.install "share-macos-aarch64" => "share"
       end
     else
-      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/sany-macos-x86_64"
-      sha256 "db302c6e1c5f56cd0ae3a44583641c6b4f11131088ce7703f7d7787b6c408a8b"
+      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/share-macos-x86_64"
+      sha256 "b6d508e73beaf34372ef4bb5f860d7056a296f7e70eea9ff2ac056939fe82f9a"
 
       def install
-        bin.install "sany-macos-x86_64" => "sany"
+        bin.install "share-macos-x86_64" => "share"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/sany-linux-aarch64"
-      sha256 "79328cafcd1de683c95b2400b6dae604b00910dea6bec30659bff2eaf245bc27"
+      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/share-linux-aarch64"
+      sha256 "712a0f92e49d160ba05fcfecfafd9bdbdf721187cbcd480ca6b58548bc82b258"
 
       def install
-        bin.install "sany-linux-aarch64" => "sany"
+        bin.install "share-linux-aarch64" => "share"
       end
     else
-      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/sany-linux-x86_64"
-      sha256 "2d04bd73668c3c70ba8e9e5b459afc7cd5f90ecb9a7b3830aca54a17ce195e8d"
+      url "https://github.com/bestdevmgp/share-anything-cli/releases/download/v#{version}/share-linux-x86_64"
+      sha256 "4c9de77ec5fddfebf532dc0f0781232d2357f910dddc062c1ce6d6f45752be50"
 
       def install
-        bin.install "sany-linux-x86_64" => "sany"
+        bin.install "share-linux-x86_64" => "share"
       end
     end
   end
 
   test do
-    assert_match "sany", shell_output("#{bin}/sany --version")
+    assert_match "share", shell_output("#{bin}/share --version")
   end
 end
